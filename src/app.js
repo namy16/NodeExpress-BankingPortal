b96de28a9da49a302296b24332b4d38a55076c46
parashan <Naman.Parashar@cdk.com>
2019-02-03 19:06:02 +0530

    created routes

--- src/app.js
+++ src/app.js
@@ -8,10 +8,32 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+const accountData = fs.readFileSync(path.join(__dirname,'json','accounts.json'), 'utf8');
+const accounts = JSON.parse(accountData);
+const userData = fs.readFileSync(path.join(__dirname,'json','users.json'), 'utf8');
+const users = JSON.parse(userData);
+
 app.get('/', function (req,res) {
-   res.render('index',{ title: 'Index' });
+   res.render('index',{ title: 'Account Summary', accounts:accounts });
+});
+
+app.get('/savings', function (req,res) {
+    res.render('account',{ account:accounts.savings });
+});
+
+app.get('/checking', function (req,res) {
+    res.render('account',{  account:accounts.checking });
+});
+
+app.get('/credit', function (req,res) {
+    res.render('account',{  account:accounts.credit });
+});
+
+app.get('/profile', function (req,res) {
+    res.render('profile',{  user:users[0] });
 });
 
 app.listen(3000,function () {
     console.log('PS Project Running on port 3000!');
-})
+});
