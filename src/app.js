66c5f85a16b370f926ee4ee6b2d5b2479506e79e
parashan <Naman.Parashar@cdk.com>
2019-02-04 11:08:42 +0530

    Completed the module

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const {accounts, users, writeJSON} = require('./data.js');
+const servicesRoutes = require('./routes/services');
+const accountRoutes = require('./routes/accounts');
 
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
@@ -16,44 +18,15 @@ app.get('/', function (req,res) {
    res.render('index',{ title: 'Account Summary', accounts:accounts });
 });
 
-app.get('/savings', function (req,res) {
-    res.render('account',{ account:accounts.savings });
-});
+app.use('/account',accountRoutes);
+app.use('/services',servicesRoutes);
 
-app.get('/checking', function (req,res) {
-    res.render('account',{  account:accounts.checking });
-});
 
-app.get('/credit', function (req,res) {
-    res.render('account',{  account:accounts.credit });
-});
 
 app.get('/profile', function (req,res) {
     res.render('profile',{  user:users[0] });
 });
 
-app.get('/transfer', function (req,res) {
-    res.render('transfer');
-});
-
-app.post('/transfer',  (req,res) => {
-    accounts[req.body.from].balance -= req.body.amount;
-    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) +
-    parseInt(req.body.amount, 10);
-    writeJSON();
-    res.render('transfer', { message: 'Transfer Completed'});
-});
-
-app.get('/payment', (req, res) => {
-    res.render('payment', {account: accounts.credit})
-});
-
-app.post('/payment',  (req,res) => {
-    accounts.credit.balance -= req.body.amount;
-    accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount);
-    writeJSON();
-    res.render('payment', { message: "Payment Successful", account: accounts.credit })
-});
 
 app.listen(3000, function () {
     console.log('PS Project Running on port 3000!');
